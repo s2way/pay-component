2561dfaf5aadfd8247fe3b168865fd01c698b0f6
Wellington Dessuy <wellington.dessuy@outlook.com>
2015-11-18 11:23:52 -0200

    Alterado o timeout default

--- src/Constants.php
+++ src/Constants.php
@@ -3,5 +3,5 @@
 define('PAY_BASE_URL', 'http://192.168.122.1:1337');
 define('METHOD_POST', 'POST');
 define('METHOD_GET', 'GET');
-define('TIMEOUT', 10);
+define('TIMEOUT', 30);
 define('STATUS_CODE_PAY_VALIDATION_ERROR', 422);
