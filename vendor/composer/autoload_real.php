51c8f8d9850bcb7ffb1d78c9d63cd430e6be1d60
Eduardo Kanno <eduardokanno@gmail.com>
2020-10-26 19:05:08 -0300

    update name and vendor files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitc09d5b3e4245acb5e1e93ec6986df43c
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -47,6 +50,24 @@ class ComposerAutoloaderInitc09d5b3e4245acb5e1e93ec6986df43c
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitc09d5b3e4245acb5e1e93ec6986df43c::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirec09d5b3e4245acb5e1e93ec6986df43c($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequirec09d5b3e4245acb5e1e93ec6986df43c($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
